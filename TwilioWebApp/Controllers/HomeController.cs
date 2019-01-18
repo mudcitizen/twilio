c6af30e5258ed303d0734cf1419e4fce6f18d90a
mudcitizen <mudcitizen@gmail.com>
2019-01-18 15:12:29 -0500

    Initial checkin

--- TwilioWebApp/Controllers/HomeController.cs
+++ TwilioWebApp/Controllers/HomeController.cs
@@ -1,16 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+using Twilio;
+using Twilio.Types;
+using Twilio.Rest.Api.V2010;
+using Twilio.AspNet.Mvc;
+using Twilio.Rest.Api.V2010.Account;
+
 namespace TwilioWebApp.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController : TwilioController
     {
         public ActionResult Index()
         {
-            return View();
+            String acctSid = ConfigurationManager.AppSettings["TwilioAccountSID"];
+            String authToken = ConfigurationManager.AppSettings["TwilioAuthToken"];
+
+            TwilioClient.Init(acctSid, authToken);
+
+            String fromPhoneNumber = ConfigurationManager.AppSettings["TwilioPhoneNumber"];
+            String toPhoneNumber = ConfigurationManager.AppSettings["MyPhoneNumber"];
+
+            string messageBody = "What's up @ " + DateTime.Now.ToShortTimeString();
+            var message = MessageResource.Create(body: messageBody,
+                from: new Twilio.Types.PhoneNumber(fromPhoneNumber),
+                to: new Twilio.Types.PhoneNumber(toPhoneNumber));
+
+            return Content(message.Sid);
         }
 
         public ActionResult About()
